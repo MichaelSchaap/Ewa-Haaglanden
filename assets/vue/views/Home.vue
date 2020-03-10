1af6f3878f12f66fdc9f697ecc716567a0eada66
Michael Schaap <37443179+Evoxius@users.noreply.github.com>
2020-03-10 22:46:10 +0100

    Added introduction section to home page

--- assets/vue/views/Home.vue
+++ assets/vue/views/Home.vue
@@ -9,6 +9,20 @@
         >
       </div>
     </section>
+
+    <section
+      id="introduction"
+      class="jumbotron text-center"
+    >
+      <div class="overlay">
+        <div class="title">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolore praesentium, quasi nihil</p>
+        </div>
+        <div class="explanation">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam sunt beatae consectetur corporis expedita obcaecati dolores, consequuntur, praesentium cupiditate sapiente aspernatur est porro soluta molestiae sequi enim! Eius, doloribus vel.</p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -21,5 +35,21 @@ export default {
 <style scoped>
 .content {
   flex: 1;
+}
+
+#introduction {
+  background-color: tomato;
+}
+
+.title {
+  font-size: 1em;
+  font-weight: bold;
+  font-family: 'Lato', 'HelveticaNeue', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  text-transform: uppercase;
+  float: none;
+  letter-spacing: 1px;
+  line-height: 1.1em;
+  text-align: center;
+
 }
 </style>
