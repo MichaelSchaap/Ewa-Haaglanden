069f7e380beb470b094e53a991abf019a8e2982d
Michael Schaap <37443179+Evoxius@users.noreply.github.com>
2020-03-10 18:43:23 +0100

    Added navigation component
    Removed navbar code from App.vue and made it its own component.

---
+++ assets/vue/components/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <router-link
+        class="navbar-brand"
+        to="/home"
+      >
+        Ewa Haaglanden
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div
+        id="navbarNav"
+        class="collapse navbar-collapse"
+      >
+        <ul class="navbar-nav">
+          <router-link
+            class="nav-item"
+            tag="li"
+            to="/home"
+            active-class="active"
+          >
+            <a class="nav-link">Home</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+  
+export default {
+  name: "Navigation",
+}
+</script>
