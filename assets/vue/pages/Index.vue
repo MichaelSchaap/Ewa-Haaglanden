537f61c14b1c5f096a576a9f54a97ef1c81de64e
Michael Schaap <37443179+Evoxius@users.noreply.github.com>
2020-03-11 21:19:38 +0100

    Updated home page

--- assets/vue/pages/Index.vue
+++ assets/vue/pages/Index.vue
@@ -13,64 +13,36 @@
 
       </div>
     </div>
-    <basic-elements></basic-elements>
-    <navigation></navigation>
-    <tabs-section></tabs-section>
-    <progress-pagination></progress-pagination>
-    <notifications></notifications>
-    <typography></typography>
-    <javascript-components></javascript-components>
-    <carousel-section></carousel-section>
-    <nucleo-icons-section></nucleo-icons-section>
+
+
+
     <div class="section">
       <div class="container text-center">
         <div class="row justify-content-md-center">
           <div class="col-md-12 col-lg-8">
-            <h2 class="title">Completed with examples</h2>
+            <h2 class="title">OPTIMALISERING AANSLUITING ONDERWIJS / ARBEIDSMARKT IN ZORG & WELZIJN.</h2>
             <h5 class="description">
-              The kit comes with three pre-built pages to help you get started
-              faster. You can change the text and images and you're good to go.
-              More importantly, looking at them will give you a picture of what
-              you can built with this powerful Bootstrap 4 ui kit.
+Extra strong is een initiatief om de aansluiting van het onderwijs op de arbeidsmarkt in de Zorg & Welzijnssector te optimaliseren. Een samenwerkingsverband om “De Nieuwe Medewerker” te creëren die klaar is voor de arbeidsmarkt van de toekomst. Extra strong is hét platform voor innovatief vakmanschap waar werkgevers, sociale partners, mbo/hbo instellingen elkaar ontmoeten.
             </h5>
           </div>
         </div>
       </div>
     </div>
-    <signup-form></signup-form>
-    <examples-section></examples-section>
+
   </div>
 </template>
 <script>
 import { Parallax } from '../components';
-import BasicElements from './components/BasicElementsSection';
-import Navigation from './components/Navigation';
-import TabsSection from './components/Tabs';
-import ProgressPagination from './components/ProgressPagination';
-import Notifications from './components/Notifications';
-import Typography from './components/Typography';
-import JavascriptComponents from './components/JavascriptComponents';
-import CarouselSection from './components/CarouselSection';
-import NucleoIconsSection from './components/NucleoIconsSection';
-import SignupForm from './components/SignupForm';
-import ExamplesSection from './components/ExamplesSection';
+
+
 
 export default {
   name: 'index',
   bodyClass: 'index-page',
   components: {
     Parallax,
-    BasicElements,
-    Navigation,
-    TabsSection,
-    ProgressPagination,
-    Notifications,
-    Typography,
-    JavascriptComponents,
-    CarouselSection,
-    NucleoIconsSection,
-    SignupForm,
-    ExamplesSection,
+
+
   }
 };
 </script>
