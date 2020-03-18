131d6146d92af6d7b17729f23ce680a46a0faeae
Michael Schaap <37443179+Evoxius@users.noreply.github.com>
2020-03-18 18:06:29 +0100

    Added NewsSection component

---
+++ assets/vue/pages/components/NewsSection.vue
@@ -0,0 +1,27 @@
+<template>
+<section style="background-color: black">
+  <div id="NewsSection" style="margin-bottom: 5px">
+     <div class="container" style="text-align: center">
+          <h2 class="title" style="color: black">NIEUWS.</h2>
+
+          <p> hi </p>
+      </div>
+
+
+  </div>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'NewsSection',
+}
+</script>
+
+<style>
+body {
+  margin: 0 !important;
+}
+</style>
