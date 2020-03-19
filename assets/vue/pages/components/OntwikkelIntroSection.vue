52ba99dfac458ffd7dc4bc69dfecc151596f6f9d
Michael Schaap <37443179+Evoxius@users.noreply.github.com>
2020-03-19 15:18:39 +0100

    Refined color and font family use

--- assets/vue/pages/components/OntwikkelIntroSection.vue
+++ assets/vue/pages/components/OntwikkelIntroSection.vue
@@ -9,17 +9,17 @@
             <div class="col-sm text-center">
                 <h5 style="font-size: 1.5em;color:black; font-weigth:bold;text-transform:uppercase;margin:0;letter-spacing:1px;text-align:center;">Ontwikkelijn A</h5>
                 <p style="text-align:center;color:black; font-size:1.1em;">Nieuwe medewerker Zorg & Welzijn</p>
-                <button type="button" class="btn btn-danger" style="text-transform:uppercase;">Meer informatie</button>
+                <button type="button" class="btn" style="text-transform:uppercase;background-color: #CC0029">Meer informatie</button>
             </div>
             <div class="col-sm text-center">
                 <h5 style="font-size: 1.5em;color:black; font-weigth:bold;text-transform:uppercase;margin:0;letter-spacing:1px;text-align:center;">Ontwikkelijn B</h5>
                 <p style="text-align:center;color:black; font-size:1.1em;">mbo & hbo ieder in z’n kracht: samen sterk</p>
-                <button type="button" class="btn btn-danger" style="text-transform:uppercase;">Meer informatie</button>
+                <button type="button" class="btn" style="text-transform:uppercase;background-color: #CC0029">Meer informatie</button>
             </div>
             <div class="col-sm text-center">
                 <h5 style="font-size: 1.5em;color:black; font-weigth:bold;text-transform:uppercase;margin:0;letter-spacing:1px;text-align:center;">Ontwikkelijn C</h5>
                 <p style="text-align:center;color:black; font-size:1.1em;">Online learning for life</p>
-                <button type="button" class="btn btn-danger" style="text-transform:uppercase;">Meer informatie</button>
+                <button type="button" class="btn" style="text-transform:uppercase;background-color: #CC0029">Meer informatie</button>
             </div>
         </div>
         </div>
--- assets/vue/pages/components/VideoSection.vue
+++ assets/vue/pages/components/VideoSection.vue
@@ -1,8 +1,8 @@
 <template>
-<section style="background-color: #CC0029">
+<section style="background-color: #FFB300">
   <div id="VideoSection" style="margin-bottom: 50px">
      <div class="container" style="text-align: center; padding-bottom:5%;">
-          <h2 class="title" style="color: white; font-size:50px;">DOEL VAN EWA-HAAGLANDEN.</h2>
+          <h2 class="title" style="color: black; font-size:50px;">DOEL VAN EWA-HAAGLANDEN.</h2>
           <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/bsPkTBn0s7I" allowfullscreen></iframe>
         </div>
