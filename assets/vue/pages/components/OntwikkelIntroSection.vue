5367b757e2b19b9198063bbdc404ccee923d256c
Michael Schaap <37443179+Evoxius@users.noreply.github.com>
2020-03-19 14:23:18 +0100

    Added OntwikkelIntroSection + now showing correct video

---
+++ assets/vue/pages/components/OntwikkelIntroSection.vue
@@ -0,0 +1,29 @@
+<template>
+<section style="background-color: #66BFFF">
+  <div id="OntwikkelIntroSection" style="margin-top:50px;margin-bottom: 50px">
+     <div class="container" style="text-align: center; margin-bottom: 30px;">
+          <h2 class="title" style="color: black; font-size:50px;">ONTWIKKELLIJNEN.</h2>
+      </div>
+      <div class="container">
+
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'OntwikkelIntroSection',
+}
+</script>
+
+<style>
+body {
+  margin: 0 !important;
+}
+.img-padding {
+  padding-left: 20px;
+}
+</style>
--- assets/vue/pages/components/VideoSection.vue
+++ assets/vue/pages/components/VideoSection.vue
@@ -4,7 +4,7 @@
      <div class="container" style="text-align: center; padding-bottom:5%;">
           <h2 class="title" style="color: white; font-size:50px;">DOEL VAN EWA-HAAGLANDEN.</h2>
           <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/bsPkTBn0s7I" allowfullscreen></iframe>
         </div>
       </div>
   </div>
