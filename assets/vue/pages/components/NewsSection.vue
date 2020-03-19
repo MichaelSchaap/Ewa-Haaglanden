e448a680e39a589e7a4bf4c93a872a1982442330
Michael Schaap <37443179+Evoxius@users.noreply.github.com>
2020-03-19 13:27:13 +0100

    Added vue NewsSection component
    The starting HTML and CSS

--- assets/vue/pages/components/NewsSection.vue
+++ assets/vue/pages/components/NewsSection.vue
@@ -1,11 +1,15 @@
 <template>
 <section style="background-color: #FFB300">
-  <div id="NewsSection" style="margin-bottom: 5px">
+  <div id="NewsSection">
      <div class="container" style="text-align: center">
-          <h2 class="title" style="color: black">NIEUWS.</h2>
-
-          <p> hi </p>
-      </div>
+          <h2 class="title" style="color: black">LAATSTE BERICHTEN.</h2>
+            <div class="card">
+              <img class="card-img" src="images/place.jfif" alt="Artikel foto">
+                <div class="card-img-overlay d-flex align-items-center container">
+                  <h5 class="card-title text-white" style="text-size:20px;" >ROC Mondriaan's innovatiehuis opent op 21 Mei!</h5>
+                </div>
+            </div>
+     </div>
 
 
   </div>
@@ -13,7 +17,6 @@
 </template>
 
 <script>
-
 
 export default {
   name: 'NewsSection',
@@ -23,5 +26,18 @@ export default {
 <style>
 body {
   margin: 0 !important;
+}
+.card-title {
+    position: absolute;
+    top: 45%;
+    left: 0;
+    width: 100%;
+    color:black;
+}
+.card-img-overlay{
+   height:100%;
+   width: 100%;
+   background-color:#000;
+   opacity:.6;
 }
 </style>
