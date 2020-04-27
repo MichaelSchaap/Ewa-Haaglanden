765ac624f2002952a6b5e8c5dcf12a8f3907cc6b
Michael Schaap <37443179+Evoxius@users.noreply.github.com>
2020-04-27 13:33:24 +0200

    Implemented pagination in partner table

--- assets/vue/pages/components/Partner/DashboardPartners.vue
+++ assets/vue/pages/components/Partner/DashboardPartners.vue
@@ -3,10 +3,27 @@
     <div class="container">
       <div class="row">
 
-
+        <div style="display:block; margin-left:2%;margin-top: 5%">
+        <button 
+        @click="prevPage"
+        type="button"
+        class="btn btn-secondary"
+        :disabled="pageNumber==0"
+        >
+        Previous
+        </button>
+        <button 
+        @click="nextPage"
+        type="button"
+        class="btn btn-secondary"
+        :disabled="pageNumber >= pageCount -1"
+        >
+        Next
+        </button>
+        </div>
 
         <table
-          style="display:block;margin-bottom:10%; margin-left:2%;margin-top: 5%"
+          style="display:block; margin-left:2%;"
           class="table table-striped table-hover table-responsive"
         >
           <thead class="thead-dark">
@@ -30,7 +47,7 @@
             </div>
 
             <div v-else-if="!hasPartners" class="row" style="margin-left:5%;">No partners!</div>
-            <tr v-for="partner in sortFunc()" v-else :key="partner.id">
+            <tr v-for="partner in paginatedData" v-else :key="partner.id">
               <td>{{partner.name}}</td>
               <td>{{partner.website}}</td>
               <td>{{partner.created.replace(/^(\d+)-(\d+)-(\d+)(.*):\d+$/, '$3/$2/$1$4').slice(0,10)}}</td>
@@ -50,6 +67,24 @@
             </tr>
           </tbody>
         </table>
+        <div style="display:block;margin-bottom:10%; margin-left:2%;">
+        <button 
+        @click="prevPage"
+        type="button"
+        class="btn btn-secondary"
+        :disabled="pageNumber==0"
+        >
+        Previous
+        </button>
+        <button 
+        @click="nextPage"
+        type="button"
+        class="btn btn-secondary"
+        :disabled="pageNumber >= pageCount -1"
+        >
+        Next
+        </button>
+        </div>
 
 
 
@@ -66,20 +101,24 @@ export default {
   components: {
     ErrorMessage
   },
+  props:{
+    size:{
+      type:Number,
+      required:false,
+      default: 10
+    }
+  },
   data() {
     return {
       id: "",
       name: "",
       website: "",
       img: "",
+      pageNumber: 0,  // default to page 0
+
     };
   },
   methods: {
-    sortFunc: function() {
-      return this.partners.slice().sort(function(a, b) {
-        return a.created < b.created ? 1 : -1;
-      });
-    },
 
     deletePartner(partnerId) {
       let i = this.partners.map(partner => partner.id).indexOf(partnerId)
@@ -92,6 +131,13 @@ export default {
     },
     goToPartner(partnerId) {
       this.$router.push({ name: "DashboardEditPartners", params: { Pid: partnerId } });
+    },
+
+    nextPage(){
+        this.pageNumber++;
+    },
+    prevPage(){
+        this.pageNumber--;
     }
 
   },
@@ -111,6 +157,18 @@ export default {
     partners() {
       return this.$store.getters["partner/partners"];
     },
+    pageCount(){
+      let l = this.partners.length,
+          s = this.size;
+      return Math.ceil(l/s);
+    },
+    paginatedData(){
+    const start = this.pageNumber * this.size,
+          end   = start + this.size;     
+    return this.partners.slice(start, end).sort(function(a, b) {
+        return a.created < b.created ? 1 : -1;
+      });
+    }
   },
   created() {
     this.$store.dispatch("partner/findAll");
